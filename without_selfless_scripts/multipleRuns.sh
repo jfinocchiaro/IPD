9e1441ad3e97f1acab60226ef24b06f4a18fba47
jfinocchiaro <22jjf22@gmail.com>
2018-10-27 11:14:13 -0600

    Beginning of cleaning up directories

--- without_selfless_scripts/multipleRuns.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for j in {1..30}; do
-	echo "Started" &
-	python without_selfless_scripts/no_selfless_no_trump_train_axelrod.py &
-done
--- without_selfless_scripts/multipleRuns_pop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for j in {1..29}; do
-	echo "Started" &
-	python without_selfless_scripts/no_selfless_no_trump_train_pop.py &
-done
