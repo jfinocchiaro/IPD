aa097e3667883caa3806208b70764d07a4f399f0
jfinocchiaro <22jjf22@gmail.com>
2018-10-27 11:35:15 -0600

    Continued cleaning up repo

--- cooperation_average/multipleRuns.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for j in {1..30}; do
-	echo "Started" &
-	python new_evolve_trump_axelrod.py &
-done
--- scale_cooperation_4_scripts/multipleRuns.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for j in {1..30}; do
-	echo "Started" &
-	python deapintrooop.py &
-done
-
